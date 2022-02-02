a90f9c6ece13614d99bd8631343d42a0cc9dfce8
Sabin Adams <sabintheworld@gmail.com>
2022-02-02 05:33:38 -0800

    Adds a rest to allow time for file to copy over

--- consolidate-prisma-binaries.sh
+++ consolidate-prisma-binaries.sh
@@ -10,3 +10,5 @@ done
 
 echo "Temporarily copying over the openssl binary to ./prisma, where it is available to every instance when searched for..."
 cp -fr ./src/generated/**/libquery_engine-rhel-openssl-1.0.x.so.node ./prisma
+
+sleep 3
